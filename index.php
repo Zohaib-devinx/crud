ee1d3921b4e00d28eca4c826fbb75a0ac17217ee
Zohaib-devinx <zohaib.haroon@devinx.com>
2024-05-20 14:47:19 +0500

    Heading changes

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
 
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Php Crud</a>
+        <h1><a class="navbar-brand" href="#">Php Crud</a></h1>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
